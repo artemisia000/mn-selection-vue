d032ad03e63ba3a0736d6f48e564a351fd4e67b1
artemisia000 <misiapierobon@gmail.com>
2026-02-04 20:47:15 +0100

    ex2 setup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app"></div>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <Exercise1 />
   <Exercise2 />
--- src/components/Exercise2.vue
+++ src/components/Exercise2.vue
@@ -1,27 +1,83 @@
 <template>
-    <div class="races">
-        auuuuuuuu
+  <section class="exercise-2" :class="customClass">
+    <div class="exercise-2__wrapper">
+      
+      <div
+        v-for="(item, index) in items"
+        :key="index"
+        class="exercise-2__item"
+        :class="item.class">
+        <component
+          v-if="item.subtitle"
+          :is="item.subtitleTag || 'h3'"
+          class="exercise-2__subtitle">
+          {{ item.subtitle }}
+        </component>
+
+        <component
+          v-if="item.title"
+          :is="item.titleTag || 'h2'"
+          class="exercise-2__title">
+          {{ item.title }}
+        </component>
+
+        <div class="exercise-2__video">
+          <iframe
+            :src="item.url"
+            frameborder="0"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowfullscreen>
+          </iframe>
         </div>
+      </div>
+
+    </div>
+  </section>
 </template>
+
 
 
 <script>
 // //import gsap from 'gsap';
 export default {
   name: 'Exercise-2',
+  props: {
+  customClass: {
+    type: String,
+    default: ''
+  },
+  items: {
+    type: Array,
+    required: true
+  }
+ }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('Exercise 2 loaded');
+});
 
 </script>
 
 
-<style scoped >
+<style scoped>
 
+.exercise-2 {
+    border: 5px solid gray;
+}
 
-.races {
-	width:fit-content;
-	display:flex;
-	flex-wrap:nowrap;
-    justify-content: space-around;
+.exercise-2__video {
+  position: relative;
+  padding-bottom: 50%;
+  height: 0;
+  overflow: hidden;
+}
+
+.exercise-2__video iframe {
+  position: absolute;
+  inset: 0;
+  width: 100%;
+  height: 100%;
 }
 
 </style>
